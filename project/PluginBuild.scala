92c89c40be6d75e3511aa44da2c3fa6458a7e090
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-10-24 17:17:47 +0100

    Update to sbt-play-artifact 2.1

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -8,7 +8,7 @@ object PluginBuild extends Build {
   // they are dependencies of the plugin itself and not associated with the 
   // build definition as plugins usually are.
 
-  val playArtifactPluginVersion = "1.8"
+  val playArtifactPluginVersion = "2.1"
 
   lazy val main = Project("sbt-play-assethash", file("."))
     // Fixed in SBT 0.12: https://github.com/harrah/xsbt/issues/329
